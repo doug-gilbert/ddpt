6a1bbeff9e1199773a171e85d424a5acfbea4525
dougg <32302704+doug-gilbert@users.noreply.github.com>
2012-03-21 10:23:36 +0000

    sync with libsgutils rev 446

--- src/ddpt.c
+++ src/ddpt.c
@@ -44,7 +44,7 @@
  * So may need CreateFile, ReadFile, WriteFile, SetFilePointer and friends.
  */
 
-static char * version_str = "0.93 20120229 [svn: r188]";
+static char * version_str = "0.93 20120320 [svn: r190]";
 
 /* Was needed for posix_fadvise() */
 /* #define _XOPEN_SOURCE 600 */
