64673d429ab8d7040bb958a1139dd738bff70813
dougg <dougg@3858cf24-66e5-4112-a7f2-057c52297c1f>
2013-09-19 21:14:22 +0000

    point svn:externals to rev 515 of sg3_utils

--- trunk/src/ddpt.c
+++ trunk/src/ddpt.c
@@ -44,7 +44,7 @@
  * So may need CreateFile, ReadFile, WriteFile, SetFilePointer and friends.
  */
 
-static const char * version_str = "0.93 20130918 [svn: r223]";
+static const char * version_str = "0.93 20130919 [svn: r224]";
 
 /* Was needed for posix_fadvise() */
 /* #define _XOPEN_SOURCE 600 */
