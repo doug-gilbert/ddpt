2967041a149162ab38b78e311ab3ef2bb9e14d46
dougg <dougg@3858cf24-66e5-4112-a7f2-057c52297c1f>
2014-06-04 00:27:27 +0000

    point svn:externals to rev 585 of sg3_utils

--- trunk/src/ddpt.c
+++ trunk/src/ddpt.c
@@ -68,7 +68,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.95 20140530 [svn: r286]";
+static const char * ddpt_version_str = "0.95 20140603 [svn: r288]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
