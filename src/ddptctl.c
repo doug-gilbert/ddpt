5b2c3951a70b0c5b76d1a8ecceef2361b91a9ac9
dougg <32302704+doug-gilbert@users.noreply.github.com>
2014-12-21 03:23:51 +0000

    point svn:externals to rev 620 of sg3_utils

--- src/ddptctl.c
+++ src/ddptctl.c
@@ -65,7 +65,7 @@
 
 #include "ddpt.h"
 
-const char * ddptctl_version_str = "0.95 20141026 [svn: r305]";
+const char * ddptctl_version_str = "0.95 20141220 [svn: r306]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
