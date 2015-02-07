495ba0b76455f919a4108e5cbb8e9fcce3654907
dougg <32302704+doug-gilbert@users.noreply.github.com>
2015-02-07 20:30:48 +0000

    fix src/Makefile.am cross-build problem

--- src/ddpt.c
+++ src/ddpt.c
@@ -68,7 +68,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.96 20150126 [svn: r309]";
+static const char * ddpt_version_str = "0.96 20150206 [svn: r310]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
