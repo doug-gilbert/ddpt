8772bac1440218ddcd76af9df7278b9949ca4336
dougg <dougg@3858cf24-66e5-4112-a7f2-057c52297c1f>
2015-11-17 19:02:50 +0000

    update dates on ddpt and ddptctl executables

--- trunk/src/ddptctl.c
+++ trunk/src/ddptctl.c
@@ -65,7 +65,7 @@
 
 #include "ddpt.h"
 
-const char * ddptctl_version_str = "0.96 20150206 [svn: r310]";
+const char * ddptctl_version_str = "0.96 20151117 [svn: r312]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
