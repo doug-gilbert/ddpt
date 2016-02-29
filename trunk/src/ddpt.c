79e9821858a94eead1cc08c9eb85cef9435b6dec
dougg <dougg@3858cf24-66e5-4112-a7f2-057c52297c1f>
2016-02-29 21:45:37 +0000

    fix pr2serr in Windows only source

--- trunk/src/ddpt.c
+++ trunk/src/ddpt.c
@@ -68,7 +68,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.96 20160225 [svn: r324]";
+static const char * ddpt_version_str = "0.96 20160229 [svn: r325]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
--- trunk/src/ddptctl.c
+++ trunk/src/ddptctl.c
@@ -66,7 +66,7 @@
 #include "ddpt.h"
 
 
-const char * ddptctl_version_str = "0.96 20160225 [svn: r324]";
+const char * ddptctl_version_str = "0.96 20160229 [svn: r325]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
