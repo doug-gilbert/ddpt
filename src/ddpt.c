e617825237c40f7ce0863cf34944f4f3e786a303
dougg <32302704+doug-gilbert@users.noreply.github.com>
2016-04-24 15:39:08 +0000

    point svn:externals to rev 693 of sg3_utils; upgrade automake to version 1.15 (U16.04)

--- src/ddpt.c
+++ src/ddpt.c
@@ -68,7 +68,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.96 20160410 [svn: r326]";
+static const char * ddpt_version_str = "0.96 20160423 [svn: r327]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
--- src/ddpt.h
+++ src/ddpt.h
@@ -45,6 +45,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <signal.h>
 #include <sys/time.h>
 
