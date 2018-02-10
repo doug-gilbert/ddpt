6680068e37a480ec17385a456f4cfaaa0a785ae8
Douglas Gilbert <dgilbert@interlog.com>
2018-02-10 03:04:00 +0000

    add status=progress option, report every 2 minutes; align pass-through buffers to page boundaries; point svn:externals to rev 751 of sg3_utils; update BSD license from 3 to 2 clause aka FreeBSD
    git-svn-id: https://svn.bingwo.ca/repos/ddpt/trunk@337 3858cf24-66e5-4112-a7f2-057c52297c1f

--- src/ddptctl.c
+++ src/ddptctl.c
@@ -1,30 +1,27 @@
 /*
- * Copyright (c) 2014-2018 Douglas Gilbert.
+ * Copyright (c) 2014-2018, Douglas Gilbert
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 /* ddpt is a utility program for copying files. It broadly follows the syntax
@@ -67,7 +64,7 @@
 #include "ddpt.h"
 
 
-const char * ddptctl_version_str = "0.96 20180113 [svn: r336]";
+const char * ddptctl_version_str = "0.96 20180209 [svn: r337]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
@@ -419,7 +416,7 @@ odx_rt_info(const struct opts_t * op)
         }
         if (op->verbose > 3) {
             pr2serr("Hex dump of chunk %d from rtf file:\n", bp_chunk);
-            dStrHexErr((const char *)rth, bp_chunk, 1);
+            hex2stderr(rth, bp_chunk, 1);
         }
         if (num > 1)
             printf("%s Decoding information from ROD Token %d\n",
@@ -484,15 +481,15 @@ report_all_toks(struct opts_t * op, struct dev_info_t * dip, int do_hex)
     }
     if (do_hex) {
         if (do_hex > 1) {
-            dStrHex((const char *)rsp, ((len < 8) ? len : 8), 1);
+            hex2stdout(rsp, ((len < 8) ? len : 8), 1);
             for (k = 0, bp = rsp + 8; k < (int)num_mtoks; ++k, bp += 96) {
                 printf("\n");
-                dStrHex((const char *)bp, 96, 1);
+                hex2stdout(bp, 96, 1);
             }
         } else  {
             if (len > sizeof(rsp))
                 len = sizeof(rsp);
-            dStrHex((const char *)rsp, len , 1);
+            hex2stdout(rsp, len , 1);
         }
         return 0;
     }
