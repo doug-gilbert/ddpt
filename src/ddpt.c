99ce2bb4943ef9e47e565331f46b4c93e6ffb384
doug-gilbert <32302704+doug-gilbert@users.noreply.github.com>
2018-03-22 00:13:25 +0100

    status=progress info
    point svn:externals to rev 760 of sg3_utils

--- src/ddpt.c
+++ src/ddpt.c
@@ -64,7 +64,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.96 20180228 [svn: r342]";
+static const char * ddpt_version_str = "0.96 20180307 [svn: r343]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
