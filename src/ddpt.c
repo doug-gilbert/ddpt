90e8486c163a98732d260ac88c02eb40907674d5
doug-gilbert <32302704+doug-gilbert@users.noreply.github.com>
2018-03-22 00:15:12 +0100

    Add little/big endian specializations to sg_unaligned.h
    point svn:externals to rev 762 of sg3_utils

--- src/ddpt.c
+++ src/ddpt.c
@@ -64,7 +64,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.96 20180307 [svn: r343]";
+static const char * ddpt_version_str = "0.96 20180316 [svn: r344]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
