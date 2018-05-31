aca306ab10882b29d0e01bf9fd4b605f0fa4c4b8
Douglas Gilbert <dgilbert@interlog.com>
2018-05-31 05:01:08 +0000

    rename REASON_UNKNOWN to avoid clash with Windows library
    git-svn-id: https://svn.bingwo.ca/repos/ddpt/trunk@355 3858cf24-66e5-4112-a7f2-057c52297c1f

--- src/ddptctl.c
+++ src/ddptctl.c
@@ -64,7 +64,7 @@
 #include "ddpt.h"
 
 
-const char * ddptctl_version_str = "0.96 20180530 [svn: r354]";
+const char * ddptctl_version_str = "0.96 20180530 [svn: r355]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
