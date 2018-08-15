5cb7cc84582a276d06a0a459258d1fbd8259f813
Douglas Gilbert <dgilbert@interlog.com>
2018-08-15 21:59:51 +0000

    point to rev 787 of sg3_utils; clean up warnings for MinGW(64 bit)
    git-svn-id: https://svn.bingwo.ca/repos/ddpt/trunk@366 3858cf24-66e5-4112-a7f2-057c52297c1f

--- src/ddpt.c
+++ src/ddpt.c
@@ -64,7 +64,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.96 20180814 [svn: r365]";
+static const char * ddpt_version_str = "0.96 20180815 [svn: r366]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
