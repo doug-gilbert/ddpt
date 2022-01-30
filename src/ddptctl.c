956bd1d4d6d9c4e4ba16d3d6e890e9c6c4b0c6bf
Douglas Gilbert <dgilbert@interlog.com>
2022-01-30 10:53:31 -0500

    sync to upstream ddpt svn rev 401
    See Changelog for pre-release ddpt-0.98 [20220129] [svn: r401]

--- src/ddptctl.c
+++ src/ddptctl.c
@@ -64,7 +64,7 @@
 #include "ddpt.h"
 
 
-const char * ddptctl_version_str = "0.98 20220109 [svn: r400]";
+const char * ddptctl_version_str = "0.98 20220129 [svn: r401]";
 
 #ifdef SG_LIB_LINUX
 #include <sys/ioctl.h>
