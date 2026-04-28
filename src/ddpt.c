863f0e562e020316e4891571591f892c2fe0ea1d
Douglas Gilbert <dgilbert@interlog.com>
2026-04-28 03:00:27 +0000

    cmake: can build deb and rpm packages with 'cpack .'
    git-svn-id: https://svn.bingwo.ca/repos/ddpt/trunk@420 3858cf24-66e5-4112-a7f2-057c52297c1f

--- src/ddpt.c
+++ src/ddpt.c
@@ -51,7 +51,7 @@
 #endif
 
 
-static const char * ddpt_version_str = "0.98 20260425 [svn: r419]";
+static const char * ddpt_version_str = "0.98 20260427 [svn: r420]";
 
 static const char * my_name = "ddpt: ";
 
--- src/ddpt_sgl.cpp
+++ src/ddpt_sgl.cpp
@@ -48,7 +48,7 @@
 #endif
 
 
-static const char * ddpt_sgl_version_str = "0.98 20260425 [svn: r419]";
+static const char * ddpt_sgl_version_str = "0.98 20260427 [svn: r420]";
 
 #include "ddpt.h"
 #include "sg_lib.h"
